cc49305e2f68cff4b8d44cba94a2cff7c7512fb9
Acsa Lu <acsa0210@gmail.com>
2013-06-21 01:00:41 +0800

    initial migrate from testing project, now can play video from markerId

--- airstorm-mothership/ASAppDelegate.h
+++ airstorm-mothership/ASAppDelegate.h
@@ -7,9 +7,20 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
 
 @interface ASAppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *markerId;
+@property (weak) IBOutlet WebView *webView;
+
+- (IBAction)runButtonPressed:(id)sender;
+
+
+- (void)detectMarkerId:(int)markerId;
+- (void)playVideoForWebView:(WebView *)webView withVideoId:(NSString *)videoId;
+- (void)showNotAssignedWarningForplayVideoForWebView:(WebView *)webView;
+- (void)createDisplayWithFrame:(CGRect)frame;
 
 @end
--- airstorm-mothership/ASAppDelegate.m
+++ airstorm-mothership/ASAppDelegate.m
@@ -7,12 +7,66 @@
 //
 
 #import "ASAppDelegate.h"
+#import <ParseOSX/Parse.h>
 
 @implementation ASAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [Parse setApplicationId:@"n7N5WY2FgddzT9GagvvgEgNFYR4u2iRjP4CkCKK3"
+                  clientKey:@"YzqN7TkJitqR9N2bCyfiaHyeJ4hM8ovEOoE69le7"];
+    
+}
+
+- (IBAction)runButtonPressed:(id)sender
+{
+    [self detectMarkerId:_markerId.intValue];
+}
+
+#pragma mark - Marker Detection methods
+
+- (void)detectMarkerId:(int)markerId
+{
+    PFQuery *query = [PFQuery queryWithClassName:@"PlayBack"];
+    
+    [query whereKey:@"markerId" equalTo:@(markerId)];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            if (objects.count == 0) {
+                NSLog(@"no video set");
+                [self showNotAssignedWarningForplayVideoForWebView:_webView];
+            } else {
+                NSLog(@"videoId: %@", objects[0][@"videoId"]);
+                [self playVideoForplayVideoForWebView:_webView withVideoId:objects[0][@"videoId"]];
+            }
+        } else {
+            NSLog(@"Error: %@", error);
+        }
+    }];
+}
+
+
+#pragma mark - Content Displaying methods
+
+- (void)showNotAssignedWarningForplayVideoForWebView:(WebView *)webView
+{
+    [webView.mainFrame loadHTMLString:@"NO MEDIA ASSIGNED YET!" baseURL:nil];
+}
+
+- (void)createDisplayWithFrame:(CGRect)frame
+{
+    
+}
+
+
+- (void)playVideoForplayVideoForWebView:(WebView *)webView withVideoId:(NSString *)videoId
+{
+    NSString *ytHTML = [NSString stringWithFormat:@"\
+                        <iframe width='%f' height='%f'\
+                        src='http://www.youtube.com/embed/%@'></iframe>", webView.frame.size.width, webView.frame.size.height, videoId];
+    
+    
+    [webView.mainFrame loadHTMLString:ytHTML baseURL:nil];
 }
 
 @end
